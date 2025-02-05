5aedb3a0a17836884921c6eef1831d1f9970c159
Davy <davysrp@gmail.com>
2025-02-05 17:21:25 +0700

    confi css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
 import './bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
 
