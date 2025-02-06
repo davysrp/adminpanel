2fd0aed5cea74bcb0ea7549d889bd8a0e8fd8a08
Davy <davysrp@gmail.com>
2025-02-06 14:27:29 +0700

    add page frontend

---
+++ app/Livewire/Frontend/PageDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Frontend;
+
+use Livewire\Component;
+
+class PageDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.page-detail');
+    }
+}
