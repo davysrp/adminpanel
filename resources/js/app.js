03cf9a9d4cccd439be6750115b4c96f9c3760278
Davy <davysrp@gmail.com>
2025-03-20 08:49:53 +0700

    remove bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-import 'bootstrap/dist/js/bootstrap';
+// import 'bootstrap/dist/js/bootstrap';
 import '../css/style.css';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
